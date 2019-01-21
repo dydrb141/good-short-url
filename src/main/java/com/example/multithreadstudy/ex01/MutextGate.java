98b2fddd1b3218b8ac8f9c9f8805d9369484da02
dragon <dydrb141@naver.com>
2019-01-21 23:45:44 +0900

    mutext lock 구현하기

---
+++ src/main/java/com/example/multithreadstudy/ex01/MutextGate.java
@@ -0,0 +1,40 @@
+package com.example.multithreadstudy.ex01;
+
+public class MutextGate {
+    private int counter = 0;
+    private String name = "Nobody";
+    private String address = "Nowhere";
+    private final Mutex mutex = new Mutex();
+
+    public void pass(String name, String address) {
+        mutex.lock();
+
+        try {
+            this.counter++;
+            this.name = name;
+            this.address = address;
+            check();
+        } finally {
+            mutex.unlock();
+        }
+    }
+
+    public String toString() {
+        String s = null;
+        mutex.lock();
+
+        try {
+            s = "NO." + counter + ": " + name + ", " + address;
+        } finally {
+            mutex.unlock();
+        }
+
+        return s;
+    }
+
+    private void check() {
+        if (name.codePointAt(0) != address.charAt(0)) {
+            System.out.printf("***Brockn ***" + toString());
+        }
+    }
+}
